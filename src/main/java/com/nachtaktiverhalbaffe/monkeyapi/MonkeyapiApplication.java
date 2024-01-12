e9587a6f590cd92f653f5660639b37e41224c72b
NachtaktiverHalbaffe <LeMonkay.VT&IT@tuanota.com>
2024-01-12 19:04:57 +0100

    Restructured project

--- src/main/java/com/nachtaktiverhalbaffe/monkeyapi/MonkeyapiApplication.java
+++ src/main/java/com/nachtaktiverhalbaffe/monkeyapi/MonkeyapiApplication.java
@@ -1,5 +1,6 @@
 package com.nachtaktiverhalbaffe.monkeyapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
