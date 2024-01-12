50642095212778be619cecbbc8557923e4b96e83
NachtaktiverHalbaffe <LeMonkay.VT&IT@tuanota.com>
2024-01-12 17:06:59 +0100

    Initialized project with spring boot intializer

---
+++ monkeyapi/src/main/java/com/nachtaktiverhalbaffe/monkeyapi/MonkeyapiApplication.java
@@ -0,0 +1,13 @@
+package com.nachtaktiverhalbaffe.monkeyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyapiApplication.class, args);
+	}
+
+}
---
+++ monkeyapi/src/test/java/com/nachtaktiverhalbaffe/monkeyapi/MonkeyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nachtaktiverhalbaffe.monkeyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
